88122b76bab469e4f0ee3f8beec4eebd56198748
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-27 13:01:46 -0300

    Header(component): Creating the Header component

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import logo from './logo.png';
+import LoginButton from '../LoginButton/LoginButton';
+
+export function Header() {
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static"
+      sx={{ background: "#fff", boxShadow: "none", border: "1px solid rgba(0, 0, 0, 0.07)", padding: "10px 120px"}}
+      >
+        <Toolbar 
+        sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+        >
+          <Typography variant="h6" component="div" sx={{ display: "flex", alignItems: "center", gap: "4px", color: "#000" }}>
+            <img src={logo} alt="" />
+            Télos
+          </Typography>
+          <LoginButton/>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
