4af5584a79e2fe1765e74e022f861301dfc4a305
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-27 14:28:56 -0300

    Home(Page):Creating the home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import FirstCard from "../../component/FirstCard/FirstCard";
+import SecondCard from "../../component/SecondCard/SecondCard";
+import img from "../../component/SecondCard/image-43.png"
+import img2 from "../../component/SecondCard/image-42.png"
+import { Header } from "../../component/Header/Header";
+import BottomCard from "../../component/BottomCard/BottomCard";
+import cardTertiary from '../../component/BottomCard/Rectangle-228.png'
+import cardSegundary from '../../component/BottomCard/Rectangle-229.png'
+import cardPrimary from '../../component/BottomCard/Rectangle-227.png'
+import img3 from '../../component/MiddleCard/image-48.png'
+import img4 from '../../component/MiddleCard/image-46.png'
+import MiddleCard from "../../component/MiddleCard/MiddleCard";
+import styles from './styles.css'
+
+export function Home() {
+    return (
+      <div className="App">
+            <Header/>
+            <div className="global">
+                    <div className="container">
+                        <FirstCard/>
+                        <div className="content">
+                            <SecondCard 
+                            text="Por que trabalhar com propósito ?"
+                            img={img}
+                            />
+                            <SecondCard 
+                            text="3 Tendência sobre o futuro do trabalho"
+                            img={img2}
+                            />
+                        </div>
+                    </div>
+                
+                    <div className="sectionCardprimary">
+                        <MiddleCard
+                            text="Negócios"
+                            img={img3}
+                        />
+                    </div>
+                    <div className="sectionCardsegundary">
+                        <MiddleCard
+                            text=""
+                            img={img4}
+                        />
+                    </div>
+
+                    <div className="footerCard">
+                        <BottomCard
+                        title = " Mercado de Trabalho"
+                        image={cardPrimary}
+                        description= "Como se comportar em uma entrevista online"
+                        />
+                        <BottomCard
+                        title = " Mercado de Trabalho"
+                        image={cardSegundary}
+                        description= "Como se destacar nas entrevista online"
+                        />
+                        <BottomCard
+                        title = " Negócios"
+                        image={cardTertiary}
+                        description= " Comunicação para venda: como ajustar seu discurso para o momento"
+                        />
+                    </div>
+            </div>
+      </div>
+    );
+  }
+  
+
