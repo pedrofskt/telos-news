f17c1c9bec2d1587213b81902d3c0e30c9aef8db
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-27 23:38:21 -0300

    Feat(Banner):Creating the BannerNewsPage

---
+++ src/component/BannerNewsPage/BannerNewsPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import img from './banner.png'
+import CardMedia from '@mui/material/CardMedia';
+import logo from './Rectangle233.png'
+import './styless.css'
+
+
+export default function Banner(){
+    return(
+        <> 
+        <CardMedia component="img" sx={{ width: "100%", height: "100vh", objectFit: "cover", position: "absolute", top: "0", zIndex: "-1"}} image={img} alt="Banner"/>
+      <div className="container1">
+        <h3>Mercado de trabalho</h3>
+        <h2>Burnout: você sabe lidar com o</h2> 
+        <h2>estomago profissional</h2>
+            <div className="content1">
+                <img src={logo} alt="" />
+                <h5>jhon Doe</h5>
+                    <div className="content1">
+                        <h5>tempo de leitura: <strong>10 minutos</strong></h5>
+                    </div>
+            </div>
+      </div>
+      </>
+    )
+}
