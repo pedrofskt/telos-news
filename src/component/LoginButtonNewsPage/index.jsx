624383429e5076bab70e9b0c5ab5ec2aacd0cf35
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-28 01:19:26 -0300

    Feat(NewButton):Creating the login button for the news page

---
+++ src/component/LoginButtonNewsPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+
+export default function LoginButtonNewsPage(){
+  return(
+    <button className="ButtonTwo">
+      Entrar
+     </button>
+  )
+}
