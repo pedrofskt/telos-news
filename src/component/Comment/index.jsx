1cb1a330e3d30166c2910bf9f2aeab1ce8e3011b
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-28 11:48:33 -0300

    Feat(comment):Creating the comment card

---
+++ src/component/Comment/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+import img from './image 53.png'
+
+
+export function Comment() {
+    return (
+        <div className="comment" >
+          <h1>Comentários</h1>
+          <img src={img} alt="imagem1"></img>
+        </div>
+        
+        
+    )
+}
