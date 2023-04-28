a866c734401071961f11345b067f00f82853a48e
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-27 21:16:59 -0300

    Feat(Routes):Creating the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Home } from "./pages/Home";
+import { News } from "./pages/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>TESTE PROJETO</h1>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} exact/>
+              <Route path="/noticia" element={<News />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
