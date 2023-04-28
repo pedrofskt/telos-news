409a5ab63dd28969de67594f608ee10428a578a1
Pedro Fernandes <pedrohq_fernandes@hotmail.com>
2023-04-27 21:53:14 -0300

    Feat(Header):Creating the News Page Header

---
+++ src/component/HeaderNewsPage/index.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+
+import LoginButton from '../LoginButton/LoginButton'
+import img from './Caminho-19043.png'
+
+
+export default function HeaderNewsPage() {
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static" sx={{background: "transparent"}}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+            
+            <Typography variant="h6" component="div">
+              Menu
+            </Typography>
+          <Typography variant="h6" component="div" sx={{display: "flex", alignItems: "center", gap: "4px"}}>
+            <img src={img} alt="Logo" />
+            Télos
+          </Typography>
+          <LoginButton />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
